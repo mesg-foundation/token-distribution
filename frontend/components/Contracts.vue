e45dbb7d09c61d4b39d3b41d162af6222b91bd27
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-02-21 15:01:16 +0700

    add basic ui

---
+++ frontend/components/Contracts.vue
@@ -0,0 +1,37 @@
+<template>
+  <vth-status @change="$emit('change')">
+    <template slot-scope="instance">
+      <vth-contract :address="token.address" :abi="token.abi">
+        <template slot-scope="MESGToken">
+          <vth-contract :address="distribution.address" :abi="distribution.abi">
+            <template slot-scope="Distribution">
+              <slot
+                :mesgToken="MESGToken"
+                :mesgAddress="token.address"
+                :distributionToken="Distribution"
+                :distributionAddress="distribution.address"
+                :instance="instance" />
+            </template>
+          </vth-contract>
+        </template>
+      </vth-contract>
+    </template>
+  </vth-status>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      token: {
+        abi: require('~/assets/abi/token.json'),
+        address: '0x420167d87d35c3a249b32ef6225872fbd9ab85d2'
+      },
+      distribution: {
+        abi: require('~/assets/abi/distribution.json'),
+        address: '0x1c86379124e27d61d3291bf865a9b1dc8b385484'
+      }
+    }
+  }
+}
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <contracts>
+        <template slot-scope="contracts">
+          <v-card v-if="contracts.instance.enabled">
+            <v-card-title class="headline">
+              MESG Token Distribution
+            </v-card-title>
+            <v-divider />
+
+            <v-card-text v-if="!canSend">
+              From: {{contracts.instance.account}}
+              <v-textarea v-model="textAddresses" labels="Distribution" hint="Format: address,amount" />
+              <v-btn color="primary" @click="prepare(contracts)">
+                Prepare
+              </v-btn>
+            </v-card-text>
+
+            <v-card-text v-else>
+              <ul>
+                <li>Total addresses: {{ addresses.length }}</li>
+                <li>Total token to send: {{ total }}</li>
+                <li>Approval: {{ allowance }}</li>
+              </ul>
+              <v-btn color="primary" v-if="needApprove" @click="approve(contracts)">
+                Approve
+              </v-btn>
+              <v-btn color="primary" v-else @click="send(contracts)">
+                Submit
+              </v-btn>
+            </v-card-text>
+
+            <v-card-text v-if="tx">
+              <vth-tx :tx="tx">
+                <template slot-scope="tx">
+                  <pre>{{ tx }}</pre>
+                </template>
+              </vth-tx>
+            </v-card-text>
+
+          </v-card>
+        </template>
+      </contracts>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import BN from 'bignumber.js'
+import Contracts from '~/components/Contracts'
+export default {
+  components: {
+    Contracts
+  },
+  data() {
+    return {
+      textAddresses: '',
+      allowance: 0,
+      canSend: false,
+      tx: null
+    }
+  },
+  computed: {
+    addresses() {
+      return this.textAddresses.split('\n').map(x => x.split(',')[0])
+    },
+    values() {
+      return this.textAddresses.split('\n').map(x => BN(x.split(',')[1]).multipliedBy(1e18))
+    },
+    total() {
+      return this.values.reduce((prev, x) => prev.plus(x), BN(0))
+    },
+    needApprove() {
+      return this.allowance.isLessThan(this.total)
+    }
+  },
+  methods: {
+    async prepare(contracts) {
+      this.allowance = BN(await contracts.mesgToken.allowance(contracts.instance.account, contracts.distributionAddress))
+      this.canSend = true
+    },
+    async approve(contracts) {
+      this.tx = await contracts.mesgToken.approve(contracts.distributionAddress, this.total.toString())
+      await this.prepare(contracts)
+    },
+    async send(contracts) {
+      this.tx = await contracts.distributionToken.distribute(this.addresses, this.values.map(x => x.toString()))
+    }
+  }
+}
+</script>
